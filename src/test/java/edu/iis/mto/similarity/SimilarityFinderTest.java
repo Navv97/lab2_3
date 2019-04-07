e917d01b6d93b2d1848d5461f64b8ce2a263d9a1
grzegorz kabzinski <grzesiek.kab@gmail.com>
2019-04-07 22:22:43 +0200

    added tests for sequence sercher dubler

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,6 +1,7 @@
 package edu.iis.mto.similarity;
 
 import edu.iis.mto.search.SearchResult;
+import edu.iis.mto.search.SequenceSearcher;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -49,5 +50,31 @@ public class SimilarityFinderTest {
         int[] collection2 = {2,3,5};
         Assert.assertThat(0.4d, is(equalTo(similarityFinder.calculateJackardSimilarity(collection1, collection2))));
     }
+
+    @Test
+    public void jackardSimilarityNumberOfCallsForFirsSequenceTest(){
+        SequenceSearcherCounterDubler sequenceSearcherCounterDubler = new SequenceSearcherCounterDubler();
+        similarityFinder = new SimilarityFinder(sequenceSearcherCounterDubler);
+        int[] collection1 = {1,2,3};
+        int[] collection2 = {1,2,3,4,5,6};
+        similarityFinder.calculateJackardSimilarity(collection1,collection2);
+        Assert.assertThat(sequenceSearcherCounterDubler.getCounter(),is(equalTo(collection1.length)));
+    }
+
+    private class SequenceSearcherCounterDubler implements SequenceSearcher {
+        private int counter = 0;
+
+        @Override
+        public SearchResult search(int key, int[] seq) {
+            this.counter++;
+            return SearchResult.builder().build();
+        }
+
+        int getCounter() {
+            return counter;
+        }
+    }
+
+
 
 }
