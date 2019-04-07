54926fd25e3f9692ff5930f921e18dc16eba3bee
grzegorz kabzinski <grzesiek.kab@gmail.com>
2019-04-07 21:27:18 +0200

    test for jackard similarity on empty collections

---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -0,0 +1,21 @@
+package edu.iis.mto.similarity;
+
+import edu.iis.mto.search.SearchResult;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+public class SimilarityFinderTest {
+    private SimilarityFinder similarityFinder;
+
+    @Test
+    public void testJackardSimilarityWithEmptyCollections() {
+        similarityFinder = new SimilarityFinder((key, seq) ->  SearchResult.builder().build());
+        int[] collection1 = {};
+        int[] collection2 = {};
+        Assert.assertThat(1.0d, is(equalTo(similarityFinder.calculateJackardSimilarity(collection1, collection2))));
+    }
+
+}
