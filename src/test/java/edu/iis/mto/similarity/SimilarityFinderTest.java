c338836452a8c88b622061d05e9c33317600fc74
grzegorz kabzinski <grzesiek.kab@gmail.com>
2019-04-07 21:36:50 +0200

    test for jackard similarity on different collections

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -11,11 +11,19 @@ public class SimilarityFinderTest {
     private SimilarityFinder similarityFinder;
 
     @Test
-    public void testJackardSimilarityWithEmptyCollections() {
+    public void testJackardSimilarityOnEmptyCollections() {
         similarityFinder = new SimilarityFinder((key, seq) ->  SearchResult.builder().build());
         int[] collection1 = {};
         int[] collection2 = {};
         Assert.assertThat(1.0d, is(equalTo(similarityFinder.calculateJackardSimilarity(collection1, collection2))));
+    }
+
+    @Test
+    public void testJackardSimilarityOnDifferentCollections() {
+        similarityFinder = new SimilarityFinder((key, seq) -> SearchResult.builder().withFound(false).build());
+        int[] collection1 = {1,2,3};
+        int[] collection2 = {4,5,6};
+        Assert.assertThat(0d, is(equalTo(similarityFinder.calculateJackardSimilarity(collection1, collection2))));
     }
 
 }
